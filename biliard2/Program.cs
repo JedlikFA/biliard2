dba6fdca0dbba823119f665dfa27abe2c8f1ad47
fekete.alexandra <fekete.alexandra@C41-12>
2020-02-18 13:50:01 +0100

    félig kész

--- biliard2/Program.cs
+++ biliard2/Program.cs
@@ -10,8 +10,45 @@ namespace biliard2
     {
         static void Main(string[] args)
         {
+            List<int> golyók = new List<int>();
+            Kezdés(golyók);
+            int leeső = LeEsőSorsoló(golyók);
+            Kiíró(golyók);
 
+           // for (int i = 0; i < golyók.Count; i++)
+           // {
+           //     Console.WriteLine(golyók[i]);
+           // }
             Console.ReadKey();
+        }
+
+        private static void Kiíró(List<int> golyók)
+        {
+            foreach (int item in golyók)
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+
+        static int LeEsőSorsoló(List<int> golyók)
+        {
+            Random rnd = new Random();
+            int szám;
+            do
+            {
+                szám = rnd.Next(golyók.Count);
+                golyók.Remove(szám);
+            } while (szám != 0);
+            return golyók[szám];
+
+        }
+
+        static void Kezdés(List<int> golyók)
+        {
+            for (int i = 1; i < 15; i++)
+            {
+                golyók.Add(i);
+            }
         }
     }
 }
